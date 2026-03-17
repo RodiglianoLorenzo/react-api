fb3e01b356884e8e8f865bad5254252ce7a91523
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-03-17 13:02:53 +0100

    complete milestone 1

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react"
+
 export default function AppMain() {
 
+    const [actors, setActors] = useState([])
 
+    function getActor() {
+
+        const api_actors = `https://lanciweb.github.io/demo/api/actors/`
+
+        fetch(api_actors)
+            .then(res => res.json())
+            .then(data => {
+                setActors(data)
+                console.log(actors)
+            })
+    }
+
+    useEffect(() => {
+        getActor()
+    }, [])
 
     return (
-        <>
+        <main>
+            <section>
+                {
+                    actors.map((actor, index) => (
 
-        </>
+                        <div key={index}>
+                            {console.log(actor)}
+
+                        </div >
+
+                    ))
+
+                }
+            </section>
+        </main >
     )
 }
