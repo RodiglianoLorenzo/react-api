7ed257c7774f3c91f42f1c91bfb75a30cdb5934c
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-03-17 15:17:00 +0100

    complete bonus1

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,7 +5,7 @@ export default function AppHeader() {
 
         <div className="text-center">
 
-            <p className="bg_Header text-white p-3 ">Wonderful Cast</p>
+            <p className="bg_Header text-white p-3 fst-italic">Wonderful Cast</p>
 
             <h1 className="fw-bolder">Actors</h1>
             <p>List of aactors fetched from an API</p>
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -3,20 +3,27 @@ import { useState, useEffect } from "react"
 export default function AppMain() {
 
     const [actors, setActors] = useState([])
-
-    function getActor() {
+    const [actresses, setActresses] = useState([])
+    function getActors() {
 
         const api_actors = `https://lanciweb.github.io/demo/api/actors/`
+        const api_actresses = `https://lanciweb.github.io/demo/api/actresses/`
 
         fetch(api_actors)
             .then(res => res.json())
-            .then(data => {
-                setActors(data)
+            .then(actors => {
+                setActors(actors)
+            })
+
+        fetch(api_actresses)
+            .then(res => res.json())
+            .then(actresses => {
+                setActresses(actresses)
             })
     }
 
     useEffect(() => {
-        getActor()
+        getActors()
     }, [])
 
     return (
@@ -25,10 +32,10 @@ export default function AppMain() {
                 <div className="container">
                     <div className="row row-cols-3">
                         {
-                            actors.map((actor, index) => (
+                            actors.map((actor) => (
 
-                                <div key={index} className="col " >
-                                    <div className="card text-center mb-2 bg_Card">
+                                <div key={actor.id} className="col my-3" >
+                                    <div className="card h-100 text-center bg_Card">
                                         <h5 className="card-title text-white">{actor.name}</h5>
                                         <img src={actor.image} className="card-img-top p-5 pb-0" alt="..." />
                                         <span className="text-white">{actor.birth_year}, {actor.nationality}</span>
@@ -42,6 +49,24 @@ export default function AppMain() {
                                     </div>
                                 </div>
 
+                            ))
+                        }
+                        {
+                            actresses.map((actresse) => (
+                                <div key={actresse.id} className="col my-3" >
+                                    <div className="card  h-100 text-center bg_Card">
+                                        <h5 className="card-title text-white">{actresse.name}</h5>
+                                        <img src={actresse.image} className="card-img-top p-5 pb-0" alt="..." />
+                                        <span className="text-white">{actresse.birth_year}, {actresse.nationality}</span>
+                                        <div className="card-body ">
+                                            <p className="card-text text-white">{actresse.biography}</p>
+                                            <div className="fw-bolder mb-0 text-danger">Known for:</div>
+                                            <p className="card-text text-danger "> {actresse.most_famous_movies.join(', ')} </p>
+                                            <div className="fw-bolder mb-0 text-warning">Awards:</div>
+                                            <p className="card-text text-warning">{actresse.awards.join(', ')}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             ))
                         }
                     </div>
